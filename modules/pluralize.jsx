70c16342b93d20a524f2b850b58200fb94a6021b
kaitoukid-1412 <phatdev.hehe@gmail.com>
2024-10-20 18:34:44 +0700

    119

--- modules/pluralize.jsx
+++ modules/pluralize.jsx
@@ -1,13 +1,23 @@
 import { useDeepCompareEffect, useRafState } from 'ahooks'
 import { tail } from 'es-toolkit'
+import { formatNumber } from 'intl-number-helper'
+import pluralize from 'pluralize'
 
-import { HoverCard, locale, MotionNumber, number, pluralize } from '../aliases'
+import { HoverCard, locale, MotionNumber, number } from '../aliases'
 
 const separator = ' '
 
-export default ({ count, word }) => {
+export default (count, word) => {
+  count = number(count)
+  word = pluralize(word, count)
+  count = formatNumber(count, locale, 's')
+
+  return `${count} ${word}`
+}
+
+export const MotionPluralize = ({ count, word }) => {
   const [state, setState] = useRafState(0)
-  const tooltip = pluralize(state, word, false)
+  const tooltip = pluralize(word, state, true)
 
   useDeepCompareEffect(() => setState(number(count)), [count])
 
